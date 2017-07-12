f99072dbfc91387eae91eaff9691f55f31485169
Clark Green <clark_green@byu.edu>
2017-07-11 22:08:51 -0600

    boiler plate

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/byuoitav/authmiddleware"
+	"github.com/byuoitav/av-api/health"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+
+	port := ":8013"
+	router := echo.New()
+	router.Pre(middleware.RemoveTrailingSlash())
+	router.Use(middleware.CORS())
+
+	// Use the `secure` routing group to require authentication
+	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+
+	//TODO add endpoints!
+
+	server := http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1024 * 10,
+	}
+
+	go health.StartupCheckAndReport()
+
+	router.StartServer(&server)
+}
