10a1ae0bef24d244ccb3ce01f99cc7f514e18bd5
Clark Green <clark_green@byu.edu>
2017-07-19 22:12:54 -0600

    which, eventually, of course, he did

--- server.go
+++ server.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/shure-audio-microservice/handlers"
+	"github.com/byuoitav/shure-audio-microservice/publishing"
+	"github.com/byuoitav/shure-audio-microservice/reporting"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
+/**
+STATES TO QUERY:
+
+	battery bars
+
+	battery time
+
+	battery type
+
+	power status
+
+STATES TO REPORT
+
+	power on
+
+	power off
+
+	RF interferance
+**/
+
+const PORT = 2202
+
 func main() {
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		errorMessage := "Could not get hostname of device: " + err.Error()
+		log.Printf(errorMessage)
+		publishing.ReportError(errorMessage)
+	}
+
+	building := strings.Split(hostname, "-")[0]
+	room := strings.Split(hostname, "-")[1]
+
+	//start live monitoring/publishing
+	go reporting.Monitor(building, room)
 
 	port := ":8013"
 	router := echo.New()
@@ -19,7 +59,7 @@ func main() {
 	// Use the `secure` routing group to require authentication
 	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
 
-	//TODO add endpoints!
+	//TODO share one connection!
 
 	secure.GET("/health", handlers.Health)
 
